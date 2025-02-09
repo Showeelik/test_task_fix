a06dd4674ac02df701c5aa6efc88f488290ca424
Showeelik <zeezxex2006@mail.ru>
2025-02-09 15:06:12 +0400

    Удаление ожидания доступности базы данных перед применением миграций в скрипте entrypoint

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
 echo "Apply database migrations"
-# Дождаться доступности базы данных
-python3 manage.py wait_for_db
-
 # Применить миграции
 python3 manage.py makemigrations
 python3 manage.py migrate

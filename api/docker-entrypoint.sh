efed7360679b75b0e54c26316fd282533a964847
Showeelik <zeezxex2006@mail.ru>
2025-02-09 15:05:50 +0400

    Обновление Dockerfile и скрипта entrypoint: добавление установки зависимостей, улучшение копирования и настройка прав, а также добавление ожидания доступности базы данных перед применением миграций.

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 echo "Apply database migrations"
+# Дождаться доступности базы данных
+python3 manage.py wait_for_db
+
+# Применить миграции
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py loaddata auth
+python3 manage.py loaddata users/fixtures/auth.json
 
 exec "$@"
